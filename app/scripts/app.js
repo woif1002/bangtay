3de55426d36e98566d89803cfc9c564abea4eac2
师企 <1002154883@qq.com>
2016-11-23 18:55:39 +0800

    原文件
    师育杰

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name xm11App
+ * @description
+ * # xm11App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('xm11App', ["textAngular"]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xm11App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xm11App
+ */
+angular.module('xm11App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
