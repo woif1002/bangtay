974daff88da695972b27183a351ffc244e34b73d
师企 <1002154883@qq.com>
2016-11-28 18:15:25 +0800

    11.28
    不合格的部分改动（登录页面,添加bug的静态页面）

--- app/scripts/controllers/zj.js
+++ app/scripts/controllers/zj.js
@@ -9,8 +9,33 @@
  */
 angular.module('xm11App')
   .controller('zjCtrl',['$rootScope','$scope','$state','$http', function($rootScope,$scope,$state,$http){
-    $scope.ff=function(e){
-    	alert(111)
-            $state.go('zj')
-    	}
+    $scope.fc=function(){
+    	if(typeof $scope.classify == 'undefined'){
+    		alert('错误分类不能为空')
+    		return false
+    	}else if(typeof $scope.frequency == 'undefined'){
+    		alert('发生频率不能为空')
+    		return false
+    	}else if(typeof $scope.importance == 'undefined'){
+    		alert('重要性不能为空')
+    		return false
+    	}else if(typeof $scope.status == 'undefined'){
+    		alert('错误状态不能为空')
+    		return false
+    	}else if(typeof $scope.summary == 'undefined' ){
+		    alert('简介内容不能为空')
+		    return false
+		    }else if(typeof $scope.description == 'undefined'){
+		    alert('描述内容不能为空')
+		    return false
+		    }
+    	$http({
+    		url:'http://www.bugcenter.com.cn:1511/item/',
+    		method:'post',
+    		data:$scope.upuser
+    	}).success(function(e){
+ 		$scope.data=e;
+            alert('恭喜您  上传成功')
+    	})
+    }
   }]);
